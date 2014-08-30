3e4df77fb58e9bb0c12dbaafad92dab24990634d
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-08-30 14:05:43 -0700

    grow and shrink $ added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello from main.js');
+function reset() {
+	setTimeout(function(){
+		$('.target').removeAttr('style');
+	}, 500)
+}
+
+
+$(document).ready(function() {
+
+	$('select').prop('selectedIndex', -1)
+
+	$('select').on('change', function() {
+		var value = $('select').val();
+		var action = value.split(' ')[0];
+		var amount = value.split(' ')[1];
+		var time = value.split(' ')[2];
+
+		var properties = {};
+		properties[action] = amount;
+
+		$('#target').animate(properties, time || 2000, reset);
+	});
+});
